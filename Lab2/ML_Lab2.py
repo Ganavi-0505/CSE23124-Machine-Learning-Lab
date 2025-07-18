2f9e7e8068ece8b1717d1311d02174bffc0567b3
Ganavi0505 <152689816+Ganavi-0505@users.noreply.github.com>
2025-07-18 09:14:22 +0000

    A4

--- Lab2/ML_Lab2.py
+++ Lab2/ML_Lab2.py
@@ -1,13 +1,15 @@
+# A1
+print("A1")
+#import 
 import numpy as np 
 import pandas as pd 
 
-# A1
-print("A1")
 # Loading Purchase data
 f=pd.read_excel("Lab Session Data.xlsx", sheet_name="Purchase data")
 # Segregating matrix a and c
 A = f[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']].values
 C = f['Payment (Rs)'].values
+
 # dimensionality of vector space
 print("Matrix A:\n", A)
 print("\n Dimensionality of A:", A.shape[1])
@@ -43,11 +45,14 @@ xtrain, xtest, ytrain, ytest, ctrain, ctest = train_test_split(
 #Training the logistic regression model
 model = LogisticRegression()
 model.fit(xtrain,ytrain)
+
 #Testing
 ypred = model.predict(xtest)
+
 # Checking accuracy
 accu = accuracy_score(ytest, ypred)
 print(f"\n Test Accuracy: {accu * 100:.2f}%")
+
 # Display output
 for name, pred in zip(ctest, ypred):
     label = le.inverse_transform([pred])[0]
@@ -69,14 +74,114 @@ import statistics
 import matplotlib.pyplot as plt
 # Load IRCTC Stock Price sheet
 stock_data = pd.read_excel("Lab Session Data.xlsx", sheet_name="IRCTC Stock Price")
+
 # Mean and Variance of Price
 prices = stock_data['Price']
 mprice = statistics.mean(prices)
 vprice = statistics.variance(prices)
 print(f"\nMean Price: {mprice:.2f}")
 print(f"Variance in Price: {vprice:.2f}")
+
 # Mean of prices on wednesdays
-wed_prices = stock_data[stock_data['Day'] == 'Wednesday']['Price']
+wed_prices = stock_data[stock_data['Day'] == 'Wed']['Price']
 mean_wed = statistics.mean(wed_prices)
 print(f"\nMean Price on Wednesdays: {mean_wed:.2f}")
-print(f"Difference from Overall Mean: {mean_wed - mean_price:.2f}")
+print(f"Difference from Overall Mean: {mean_wed - mprice:.2f}")
+
+# Mean of prices in April
+stock_data['Date'] = pd.to_datetime(stock_data['Date'])
+aprices = stock_data[stock_data['Date'].dt.month == 4]['Price']
+if aprices.empty:
+    print("No April data found.")
+else:
+    mean_april = statistics.mean(aprices)
+    print(f"\nMean Price in April: {mean_april:.2f}")
+    print(f"Difference from Overall Mean: {mean_april - mprice:.2f}")
+
+# Probability of Loss
+chg = stock_data['Chg%'].astype(str).str.replace('%', '', regex=False).astype(float)
+prob_loss = (chg < 0).sum() / len(chg)
+print(f"\nProbability of Making a Loss: {prob_loss:.2f}")
+
+# Probability of profit on wed
+wed_chg = stock_data[stock_data['Day'] == 'Wed']['Chg%'].astype(str).str.replace('%', '', regex=False).astype(float)
+if wed_chg.empty:
+    print("No Wednesday Chg% \data found.")
+else:
+    prob_profit_wed = (wed_chg > 0).sum() / len(wed_chg)
+    print(f"\nProbability of Profit on Wednesdays: {prob_profit_wed:.2f}")
+
+# Scatter plot
+chg_clean = stock_data['Chg%'].astype(str).str.replace('%', '', regex=False).astype(float)
+days = stock_data['Day'] 
+
+# Assigning numerical value to days for plotting
+unique_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+day_to_num = {day: i for i, day in enumerate(unique_days)}
+day_nums = days.map(day_to_num)
+
+# Plot
+plt.figure(figsize=(8, 5))
+plt.scatter(day_nums, chg_clean, color='teal', alpha=0.7)
+plt.xticks(ticks=range(len(unique_days)), labels=unique_days)
+plt.xlabel("Day of the Week")
+plt.ylabel("Chg%")
+plt.title("IRCTC Stock Chg% vs Day (Raw Data)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+plt.savefig("chg_percent_vs_day.png")
+print("\n✅ Plot saved as 'chg_percent_vs_day.png'")
+
+# A4
+print("\n\n")
+
+# Loading data
+thyroid_data = pd.read_excel("Lab Session Data.xlsx", sheet_name="thyroid0387_UCI")
+
+
+# identifying datatypes 
+print("\nData Types:")
+print(thyroid_data.dtypes)
+
+# Categorical attribute encoding scheme
+# Label Encoding if there are 3 or fewer unique values else One-Hot Encoding.
+cat_cols = thyroid_data.select_dtypes(include='object').columns.tolist()
+for col in cat_cols:
+    unique_vals = thyroid_data[col].nunique()
+
+    if unique_vals <= 3:
+        encoding = 'Label Encoding'
+    else:
+        encoding = 'One-Hot Encoding'
+    
+    print(f"{col}: {encoding}")
+
+# Data range for numeric values
+print("\nSummary of Numeric Columns:")
+print(thyroid_data.describe())
+
+# Missing values
+print("\nMissing Values:")
+print(thyroid_data.isnull().sum())
+
+# Outlier detection using IQR [uses median based quartiles which are more stable in skewed data]
+print("\nOutliers (IQR method):")
+numeric_cols = thyroid_data.select_dtypes(include='number').columns
+for col in numeric_cols:
+    q1 = thyroid_data[col].quantile(0.25)
+    q3 = thyroid_data[col].quantile(0.75)
+    iqr = q3 - q1
+    outliers = ((thyroid_data[col] < (q1 - 1.5 * iqr)) | (thyroid_data[col] > (q3 + 1.5 * iqr))).sum()
+    print(f"{col}: {outliers} outliers")
+
+# Mean, Variance and standard deviation
+print("\nVariance and Standard Deviation of Numeric Columns:")
+print("\nMean, Variance, and Standard Deviation of Numeric Columns:")
+for col in numeric_cols:
+    mean = thyroid_data[col].mean()
+    var = thyroid_data[col].var()
+    std = thyroid_data[col].std()
+    print(f"{col}: Mean = {mean:.2f}, Variance = {var:.2f}, Std Dev = {std:.2f}")
+
+
